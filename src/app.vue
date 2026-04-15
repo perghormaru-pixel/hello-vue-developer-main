c4c47c2ae4d9e0e84993680ccd008ae2da40b122
alumnos <perghormaru@gmail.com>
2026-04-15 10:59:10 -0500

    feat(developer ) : add a developer greeting

--- src/app.vue
+++ src/app.vue
@@ -1,34 +1,44 @@
 <script setup>
-import {ref} from 'vue'
+import {ref} from "vue";
+import {Developer} from "./greetings/domain/model/developer.entity.js";
+import DeveloperRegistration from "./greetings/presentation/components/developer-registration.vue";
 import DeveloperCountShow from "./greetings/presentation/components/developer-count-show.vue";
+import DeveloperGreeting from "./greetings/presentation/components/developer-greeting.vue";
 
 const firstName = ref('');
 const lastName = ref('');
 const developerCount = ref(0);
 const hasRegistered = ref(false);
 
-function updaeRegisteredDeveloperInfor(developer){
+function updateRegisteredDeveloperInfo(developer) {
+  firstName.value = developer.firstName;
+  lastName.value = developer.lastName;
+  hasRegistered.value = true;
+  console.log('Developer registered: ', developer);
+  updateDeveloperCount(developer);
+}
+
+function resetRegisteredDeveloperInfo() {
   firstName.value = '';
   lastName.value = '';
   hasRegistered.value = false;
   console.log('Registration deferred');
 }
 
-function updateDeveloperCount(developer){
-  const dev= new Developer(developer.firstName, developer.lastName);
-  if(dev.fullName !== 'Unkown') developerCount.value++;
-
+function updateDeveloperCount(developer) {
+  const dev = new Developer(developer.firstName, developer.lastName);
+  if (dev.fullName !== 'Unknown') developerCount.value++;
 }
 
 </script>
 
 <template>
   <h1>Hello Vue Developer Application</h1>
-  <developer-count
-   @developer-registered="updateRegisteredDeveloperInfo"
-   @registration-deferred="resetRegisteredDeveloperInfo"
+  <developer-registration
+      @developer-registered="updateRegisteredDeveloperInfo"
+      @registration-deferred="resetRegisteredDeveloperInfo"
   />
+  <developer-greeting v-if="hasRegistered" :first-name="firstName" :last-name="lastName"/>
   <developer-count-show :developer-count="developerCount"/>
-
 
 </template>

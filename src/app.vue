4386d023fca9c9919dcffa15ccca30aa687f8adc
alumnos <perghormaru@gmail.com>
2026-04-15 10:45:07 -0500

    feat(developer-count) : add a developer count

--- src/app.vue
+++ src/app.vue
@@ -1,7 +1,34 @@
 <script setup>
+import {ref} from 'vue'
+import DeveloperCountShow from "./greetings/presentation/components/developer-count-show.vue";
+
+const firstName = ref('');
+const lastName = ref('');
+const developerCount = ref(0);
+const hasRegistered = ref(false);
+
+function updaeRegisteredDeveloperInfor(developer){
+  firstName.value = '';
+  lastName.value = '';
+  hasRegistered.value = false;
+  console.log('Registration deferred');
+}
+
+function updateDeveloperCount(developer){
+  const dev= new Developer(developer.firstName, developer.lastName);
+  if(dev.fullName !== 'Unkown') developerCount.value++;
+
+}
 
 </script>
 
 <template>
+  <h1>Hello Vue Developer Application</h1>
+  <developer-count
+   @developer-registered="updateRegisteredDeveloperInfo"
+   @registration-deferred="resetRegisteredDeveloperInfo"
+  />
+  <developer-count-show :developer-count="developerCount"/>
+
 
 </template>
